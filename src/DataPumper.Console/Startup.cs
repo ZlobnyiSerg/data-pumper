79220aaafbe5199548d3f5bcfd758a4279444075
Алексей Федоренко <fawgv@mail.ru>
2020-07-02 15:43:21 +0300

    Добавил HangFire

---
+++ src/DataPumper.Console/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire;
+using Owin;
+
+namespace DataPumper.Console
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Map Dashboard to the `http://<your-app>/hangfire` URL.
+            app.UseHangfireDashboard("");
+        }
+    }
+}
---
+++ src/DataPumper.Console/UnityJobActivator.cs
@@ -0,0 +1,50 @@
+﻿using Hangfire;
+using Microsoft.Practices.Unity;
+using System;
+
+namespace DataPumper.Console
+{
+    internal class UnityJobActivator : JobActivator
+    {
+        private IUnityContainer container;
+
+        public UnityJobActivator(IUnityContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException("container");
+
+            this.container = container;
+        }
+
+        /// <inheritdoc />
+        public override object ActivateJob(Type jobType)
+        {
+            return this.container.Resolve(jobType);
+        }
+
+        public override JobActivatorScope BeginScope(JobActivatorContext context)
+        {
+            return new UnityScope(container.CreateChildContainer());
+        }
+
+        class UnityScope : JobActivatorScope
+        {
+            private readonly IUnityContainer container;
+
+            public UnityScope(IUnityContainer container)
+            {
+                this.container = container;
+            }
+
+            public override object Resolve(Type type)
+            {
+                return container.Resolve(type);
+            }
+
+            public override void DisposeScope()
+            {
+                container.Dispose();
+            }
+        }
+    }
+}
