4d09216616c15e7e518cee1c41b01fe0abc4d2a8
Alexey Kalinin <fiu.tee@yandex.ru>
2020-09-09 22:03:57 +0300

    test logs

--- src/Quirco.DataPumper/DataLayer/DataPumperContext.cs
+++ src/Quirco.DataPumper/DataLayer/DataPumperContext.cs
@@ -11,13 +11,16 @@ namespace Quirco.DataPumper.DataLayer
 {
     public class DataPumperContext : DbContext
     {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(DataPumperService));
         public DbSet<TableSync> TableSyncs { get; set; }
 
         public DbSet<JobLog> Logs { get; set; }
 
         public DataPumperContext() : this(new DataPumperConfiguration().ConnectionString)
         {
+            var test = new DataPumperConfiguration();
 
+            Log.Warn("!!!!!!!!!!!!!!!!! "+test.ConnectionString);
         }
 
         public DataPumperContext(string connectionString) : base(connectionString)
