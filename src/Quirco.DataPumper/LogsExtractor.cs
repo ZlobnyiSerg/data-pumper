ad5ab290f5d33f80d592495eea2c65a1ca124aed
Alexey <fiu.tee@yandex.ru>
2020-09-09 13:12:17 +0300

    build commit v0.7.3

---
+++ src/Quirco.DataPumper/LogsExtractor.cs
@@ -0,0 +1,20 @@
+﻿using Quirco.DataPumper.DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quirco.DataPumper
+{
+    public class LogsExtractor
+    {
+        public IEnumerable<JobLog> GetLogs(int count)
+        {
+            using(var ctx = new DataPumperContext())
+            {
+                return ctx.Logs.Take(count);
+            }
+        }
+    }
+}
