6b248cba70161817e21753080086349b69c4ef59
ZlobnyiSerg <zlobnyiserg@gmail.com>
2021-09-15 17:19:27 +0300

    Миграции: добавлена опция для потери данных при миграции

--- src/Quirco.DataPumper/Migrations/Configuration.cs
+++ src/Quirco.DataPumper/Migrations/Configuration.cs
@@ -9,6 +9,7 @@ namespace Quirco.DataPumper.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(DataPumperContext context)
