191b8fc828e31bcd6ecfd285c03deb7a7cbd0eff
kashelkin <40003924+kashelkin@users.noreply.github.com>
2023-03-03 12:36:05 +0300

    DataPumper: не работает с сервером postgresql #Libra-13339 fixed

--- src/DataPumper.Console/Bootstrapper.cs
+++ src/DataPumper.Console/Bootstrapper.cs
@@ -1,16 +1,24 @@
 ﻿using DataPumper.Core;
+using DataPumper.PostgreSql;
 using DataPumper.Sql;
 using Microsoft.Practices.Unity;
 using Quirco.DataPumper;
+using System;
 using System.Collections.Generic;
-using DataPumper.PostgreSql;
 
 namespace DataPumper.Console
 {
     public class Bootstrapper
     {
+        private const string SqlTargetProvider = "SqlServer";
+        private const string PostgresTargetProvider = "PostgreSQL";
+
         public static void Initialize(IUnityContainer container, WarehouseServiceConfiguration config)
         {
+            if (config.TargetProvider != SqlTargetProvider && config.TargetProvider != PostgresTargetProvider)
+                throw new ApplicationException(
+                    $"Wrong TargetProvider: {config.TargetProvider}. Supported TargetProviders: {SqlTargetProvider}, {PostgresTargetProvider}.");
+
             container.RegisterType<DataPumperService>();
             container.RegisterType<Core.DataPumper>();
 
@@ -20,8 +28,8 @@ namespace DataPumper.Console
 
             // IDataPumperTarget
             container.RegisterType<IEnumerable<IDataPumperTarget>, IDataPumperTarget[]>();
-            container.RegisterType<IDataPumperTarget, PostgreSqlDataPumperTarget>("PostgreSQL");
-            container.RegisterType<IDataPumperTarget, SqlDataPumperSourceTarget>("SqlServer");
+            container.RegisterType<IDataPumperTarget, PostgreSqlDataPumperTarget>(PostgresTargetProvider);
+            container.RegisterType<IDataPumperTarget, SqlDataPumperSourceTarget>(SqlTargetProvider);
         }
     }
 }
